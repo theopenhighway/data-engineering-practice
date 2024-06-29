d86251a54af89813aa9e2e936163b2935ad14965
theopenhighway <earleen.525190010@stu.untar.ac.id>
2024-06-29 12:41:17 +0700

    fix: create_directory generate error code if dir already exists

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import zipfile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,24 +12,47 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+parent_dir = "C:\\Users\\milo\\personal projects\\data-engineering-practice\\Exercises\\"
 
 def create_directory(dir):
-    parent_dir = "C:\\Users\\milo\\personal projects\\data-engineering-practice\\Exercises\\"
     path = os.path.join(parent_dir, dir)
 
     if not os.path.exists(path):
         try:
             os.mkdir(path)
             print("Directory '%s' created" % dir)
+            return
         except OSError as error:
-            print("error:" %  error)
+            print("error: %s" %  error)
+            return
+    
+    print("Directory '%s' already exists" % dir)
+
+def download_files(uri):
+    query_parameters = {"downloadformat": "zip"}
+
+    try:
+        response = requests.get(uri, params=query_parameters)
+    except requests.exceptions.HTTPError as e:
+        print(e.response.text)
+        return
+    
+    with open("gdp_by_country.zip", mode="wb") as file:
+        file.write(response.content)
+    
+    
+def unzip_files(filename):
+    path_to_zip_file = parent_dir + "\\downloads\\" + filename
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(path_to_zip_file)
 
 def main():
     dir = "downloads"
 
     create_directory(dir)
-    # your code here
-    pass
+    # for uri in download_uris:
+    #     download_files(uri)
+
 
 
 if __name__ == "__main__":
