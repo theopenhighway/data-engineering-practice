43fc79c6ab6264e3d547740a2c9ff30cf17536c7
theopenhighway <earleen.525190010@stu.untar.ac.id>
2024-06-29 16:56:47 +0700

    feat: find filename for the specific date

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,10 +1,35 @@
 import requests
 import pandas
+from bs4 import BeautifulSoup
 
+def find_filename(soup, date):
+    for i in range(3,10):
+        tr = soup.find_all('tr')[i]
+        date_time = tr.find_all('td')[1]
+        
+        if date_time.get_text(strip=True) == date:
+            filename_attribute = tr.find_all('td')[0].find('a')
+            return filename_attribute.get_text()
+    
+    return None
+
+def download_csv(url):
+    pass
+
+def csv_to_pandas():
+    pass
 
 def main():
     # your code here
-    pass
+    url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    date = "2024-01-19 09:53"
+    filename = find_filename(soup, date)
+
+    download_link = url + filename
+
+    download_csv(download_link)
 
 
 if __name__ == "__main__":
